d0e18e200650ee437a0dd4d4e32308e370c91095
klopstock-dviz <60588692+klopstock-dviz@users.noreply.github.com>
2020-03-11 17:09:35 +0100

    Add files via upload

--- gestionMap_2.js
+++ gestionMap_2.js
@@ -22,6 +22,8 @@
 		if (list_markers.length > 0) {
 			addMarkers();
 			setBounds();
+			init_vlSpec_legend_map_1();
+			init_vlSpec_tick_prix_m2();
 
 		}
 
@@ -29,5 +31,32 @@
 		//augmenter zoom d'un point
 /*		zoomActuel = mymap1.getZoom() 
 		mymap1.setZoom(zoomActuel-1) */
-	},2000 )
+	},4000 )
 
+
+
+function init_map() {
+
+		mymap1.fitBounds([[51.072228, 2.528016], [42.442288, 3.159714]]);
+
+
+		function setBounds() {
+			mymap1.flyToBounds(list_lat_lng);
+			
+		};
+
+
+		function addMarkers() {
+			layerGroup1 = L.layerGroup(list_markers);
+			layerGroup1.addTo(mymap1);
+		};
+
+		if (list_markers.length > 0) {
+			addMarkers();
+			setBounds();
+
+		}
+
+}
+
+import * as adapt_tailleCercles_zoom from 'adapt_tailleCercles_zoom.js';
